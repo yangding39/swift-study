0673d7b6c1f86464e1840069ac8bc57355f90759
IMEDS <jchangsr@chenze.local>
2014-06-28 17:16:34 +0800

    Initial Commit

---
+++ swift-study/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift-study
+//
+//  Created by IMEDS on 14-6-28.
+//  Copyright (c) 2014年 IMEDS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
